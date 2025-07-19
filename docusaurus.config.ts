37ead6b91cd75ccb97e42715dc259841c9defdac
melonlorrd <mprasadme@gmail.com>
2025-07-19 23:14:31 +0530

    chore: update info

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,11 +5,11 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'snwzd.com',
-  favicon: 'img/snwzd.png',
+  title: 'melonlorrd',
+  favicon: 'img/melonlorrd.png',
 
   // Set the production url of your site here
-  url: 'https://snwzd.com',
+  url: 'https://melonlorrd.snwzd.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
